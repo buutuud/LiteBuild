9be8a569a7196e170e2652ab67fcf450fe134f5d
happyyi <buutuud@gmail.com>
2017-03-25 00:38:07 +0800

    prefab使用mv命令

--- merge.sh
+++ merge.sh
@@ -2,15 +2,29 @@
 s='trunk'
 d='applerelease'
 
+promptValue() {
+    read -p "$1:" input </dev/tty
+    echo $input
+}
+
 function cmd_A
 {
-    unset dorm
     echo "Do you want to move the file $1 > $2? (Y/N) (default: Y) __"
-    read dorm
-    echo $dorm
-    dorm=${dorm:=Y}
-    if [[ $dorm -eq Y ]]; then
+    YN=$(promptValue "Enter Y/N")
+    if [[ $YN == Y ]]; then
         cp $1 $2
+    fi
+}
+
+function cmd_MV
+{
+    echo "Do you want to move the file $1 > $2? (Y/N) (default: Y) __"
+    YN=$(promptValue "Enter Y/N")
+    if [[ $YN == Y ]]; then
+        echo "CMD>> mv $1 $2"
+        rm $2 && cp $1 $2
+    else
+        echo 'NO'
     fi
 }
 
@@ -18,8 +32,13 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     f=${line:16}
     l=$s/$f r=$d/$f
     if [[ -e $l ]] && [[ -e $r ]]; then
-        echo "BCompare $s/$f $d/$f"
-        BCompare $s/$f $d/$f
+        file_type=${l#*.}
+        if [[ $file_type == 'cs' ]]; then
+            echo "CMD>> BCompare $s/$f $d/$f"
+            BCompare $s/$f $d/$f
+        elif [[ $file_type == 'prefab' ]]; then
+            cmd_MV $l $r
+        fi
     elif [[ -ne $r ]] && [[ -e $l ]]; then
         cmd_A $l $r
     else
